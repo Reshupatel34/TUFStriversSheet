6483b93ceafced23e9e5f4ea75cc45312db06128
Reshu Patel <reshu.patel_cs23@gla.ac.in>
2026-03-12 22:07:19 +0530

    "Binary Search Iterative approach"

--- src/BinarySearchIterative.java
+++ src/BinarySearchIterative.java
@@ -1,2 +1,24 @@
+
 public class BinarySearchIterative {
+    public static void main(String[] args) {
+        int[] nums={3,4,5,7,9,12,16,17};
+        int target=17;
+        int low=0;
+        int high=nums.length-1;
+
+        while(low<=high){
+            int mid=(high+low)/2;
+            if(nums[mid]==target){
+                System.out.println("Found at index : "+mid);
+                break;
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else if(nums[mid]<target){
+                low=mid+1;
+            }
+        }
+
+    }
 }
